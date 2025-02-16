5a952205fe7cf052c7a06eaab513d3daba8888d6
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-02-16 12:46:16 +0900

    TODOリストにドラッグ＆ドロップ機能を追加

--- next_todo_ghca/app/page.tsx
+++ next_todo_ghca/app/page.tsx
@@ -1,6 +1,59 @@
 "use client";
 
 import React, { useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const ItemType = "TODO_ITEM";
+
+interface TodoItemProps {
+  todo: string;
+  index: number;
+  moveTodo: (fromIndex: number, toIndex: number) => void;
+  deleteTodo: (index: number) => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  index,
+  moveTodo,
+  deleteTodo,
+}) => {
+  const [, ref] = useDrag({
+    type: ItemType,
+    item: { index },
+  });
+
+  const [, drop] = useDrop({
+    accept: ItemType,
+    hover: (draggedItem: { index: number }) => {
+      if (draggedItem.index !== index) {
+        moveTodo(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  return (
+    <li
+      ref={(node) => {
+        ref(drop(node));
+      }}
+    >
+      {todo}
+      <button
+        onClick={() => deleteTodo(index)}
+        style={{
+          marginLeft: "10px",
+          border: "1px solid black",
+          padding: "5px 10px",
+        }}
+      >
+        削除
+      </button>
+    </li>
+  );
+};
 
 const Home = () => {
   const [todos, setTodos] = useState<string[]>([]);
@@ -18,54 +71,57 @@ const Home = () => {
     setTodos(newTodos);
   };
 
+  const moveTodo = (fromIndex: number, toIndex: number) => {
+    const updatedTodos = [...todos];
+    const [movedTodo] = updatedTodos.splice(fromIndex, 1);
+    updatedTodos.splice(toIndex, 0, movedTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <h1>TODOリスト</h1>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="新しいTODOを入力"
-          style={{ color: "black" }}
-        />
-        <button
-          onClick={addTodo}
-          style={{
-            marginLeft: "10px",
-            border: "1px solid black",
-            padding: "5px 10px",
-          }}
-        >
-          追加
-        </button>
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <h1>TODOリスト</h1>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="新しいTODOを入力"
+            style={{ color: "black" }}
+          />
+          <button
+            onClick={addTodo}
+            style={{
+              marginLeft: "10px",
+              border: "1px solid black",
+              padding: "5px 10px",
+            }}
+          >
+            追加
+          </button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <TodoItem
+              key={index}
+              index={index}
+              todo={todo}
+              moveTodo={moveTodo}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
       </div>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <button
-              onClick={() => deleteTodo(index)}
-              style={{
-                marginLeft: "10px",
-                border: "1px solid black",
-                padding: "5px 10px",
-              }}
-            >
-              削除
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </DndProvider>
   );
 };
 
